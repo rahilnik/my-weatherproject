ed517831d621af93ee8c721d67c82e14529e7330
rahilnik <111749353+rahilnik@users.noreply.github.com>
2022-09-01 04:15:26 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,39 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
-		<link rel="stylesheet" href="style.css" />
-		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <title>Weather App</title>
-  </head>
+var now = new Date();
 
-<body>
-	<div id="app"></div>
-	<div class="weatherapp">
-<h1>kermanshah tehran ramsar paris </h1>
-<h2> kermanshah</h2>
-<div class="clearfix weather-temperature">
-	<span id="temperature"> 33°C </span><span class="units">
-		
-		<a href="#" id="celsius-link">°C</a> |
-		<a href="#" id="fahrenheit-link">°F</a>
-		<div>
-			<span id="description">mostly cloudy</span
-			><img src="" alt="Clear" id="icon" />
-		  </div>
-	</span>
-</div>
-	<div class="weather-forecast" id="forecast"></div>
-	<ul>
-		<li>Humidity: <span id="humidity"></span>%</li>
-		<li>Wind: <span id="wind"></span> km/h</li>
-	</ul>
-		<h3> today<
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+var day = days[now.getDay()];
+let h3 = document.querySelector("h3");
+h3.innerHTML = `today is ${day} ${hours}:${minutes}`;
+
+
+        
+
+function searchcity(city) {
+  let apiKey = "9afbead6b698cb67e5e11d1dfdb440d5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-submit").value;
+  searchcity(city);
+}
+function displayWeatherCondition(response) {
+  document.querySelector("h2").innerHTML = response.data.name;
+  let tempelement= document.querySelector("#temperature");
+   celsiusTemperature = response.data.main.temp;
+  tempelement.innerHTML= Math.round(celsiusTemperature);
+  
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+ 
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+  }
+  
+
+
+function displayfahrenheittemperature(event){
+  event.preventDefault();
+  let tempelement= document.querySelector("#temperature");
+  let fahrenheittemperature=(celsiusTemperature*9)/5 + 32;
+  tempelement.innerHTML= Math.round(fahrenheittemperature);
+}
+function displaycelsuistemperature(event){
+  event.preventDefault();
+  let tempelement= document.querySelector("#temperature");
+  tempelement.innerHTML= Math.round(celsiusTemperature);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+let celsiusTemperature =null;
+let fahrenheitlink = document.querySelector("#fahrenheit-link");
+fahrenheitlink.addEventListener("click", displayfahrenheittemperature);
+let celsiuslink = document.querySelector("#celsius-link");
+celsiuslink.addEventListener("click", displaycelsuistemperature);
