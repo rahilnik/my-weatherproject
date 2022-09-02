046350f51f834f56fea66bf2f2d09f3037653e32
rahilnik <111749353+rahilnik@users.noreply.github.com>
2022-09-02 10:10:14 -0700

    Update index.js
    function get forecast added

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
+function getforecst(coordinates){
+  console.log(coordinates);
+  let apiKey = "9afbead6b698cb67e5e11d1dfdb440d5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
         
 
@@ -83,7 +89,7 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
- 
+  getforecst(response.data.coord);
   }
   
 
