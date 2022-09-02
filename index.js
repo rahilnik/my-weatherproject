9cda69daaec288be8023243e3326f4cbb6e94745
rahilnik <111749353+rahilnik@users.noreply.github.com>
2022-09-02 10:38:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -52,15 +52,16 @@ function displayForecast() {
 }
 function getforecst(coordinates){
   console.log(coordinates);
-  let apiKey = "9afbead6b698cb67e5e11d1dfdb440d5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579"
+  let apiUrl = `https://api.openweathermap.org/data/2.5 /onecall?lat=${cordinates.lat}&lon=${cordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
+  
 }
 
         
 
 function searchcity(city) {
-  let apiKey = "9afbead6b698cb67e5e11d1dfdb440d5";
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
