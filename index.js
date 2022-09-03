6db19f929dd4cf8d245f2ad8379b8b7b8aa7582d
rahilnik <111749353+rahilnik@users.noreply.github.com>
2022-09-03 03:54:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ let days = [
 let day = days[date.getDay()];
 let h3 = document.querySelector("h3");
 h3.innerHTML = `today is ${day} ${hours}:${minutes}`;
-function displayForecast(response) {
-  let forecast = response.data.daily;
+function displayForecast(respons) {
+  
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -54,7 +54,7 @@ function displayForecast(response) {
 function getforecst(coordinates){
   console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579"
-  let apiUrl = `https://api.openweathermap.org/data/2.5 /onecall?lat=${cordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
   
 }
